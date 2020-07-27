faaeaea61f1e3a5dee6bc9205c243846a03131de
Νικόλαος Πασχάλης <n.pashalis@DYPE.LOCAL>
2020-07-27 14:12:22 +0300

    Updates

---
+++ precache-manifest.54500a8c73dd5599e7250bfff44bee24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83e674b71c9e93b05b64d60ef484a04e",
+    "url": "/comic-bot/index.html"
+  },
+  {
+    "revision": "4ff69205ecd9fe106816",
+    "url": "/comic-bot/static/css/main.85455220.chunk.css"
+  },
+  {
+    "revision": "34f55ca299e171d21ac4",
+    "url": "/comic-bot/static/js/2.1fef8ab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/comic-bot/static/js/2.1fef8ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ff69205ecd9fe106816",
+    "url": "/comic-bot/static/js/main.9b286cb3.chunk.js"
+  },
+  {
+    "revision": "ce8d1063a1719075ba54",
+    "url": "/comic-bot/static/js/runtime-main.96902a49.js"
+  }
+]);
